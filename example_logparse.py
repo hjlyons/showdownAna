0d68d45c3338d19b4cc4b2b43f015d8bf210f70d
Harry John Lyons <hlyons1994@gmail.com>
2021-02-15 14:53:57 +0000

    Add initial team parsing

--- example_logparse.py
+++ example_logparse.py
@@ -7,7 +7,9 @@ def main():
     test_files = glob.glob("example_logs/*.txt")[:10]
     for logfile in test_files:
         print("\nParsing Logfile: {}".format(logfile))
-        dumy_log = ShowdownLog(logfile)
-    
+        dummy_log = ShowdownLog(logfile)
+        dummy_log.get_initialteams()
+
+
 if __name__ == "__main__":
     main()
--- src/ShowdownLog.py
+++ src/ShowdownLog.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+TODO: If Increased performance needed, may convert string checks into regex
+"""
 
 class ShowdownLog:
     def __init__(self, infile):
@@ -20,13 +23,32 @@ class ShowdownLog:
         self.turn_indexlist = [i for i, line in enumerate(self.log_lines) if "|turn|" in line]
         assert len(self.turn_indexlist) > 0
 
-        # Print of collected information
+        self.teampreview_indexlist = [i for i, line in enumerate(self.log_lines) if "|poke|p" in line]
+        
+
+
+    def print_indexvals(self):
+        """ Print of collected information """
+        print("TeamPreview Index List: {}".format(self.teampreview_indexlist))
         print("Start Index: {}".format(self.start_index))
-        print(self.log_lines[(self.start_index)])
         print("Turn Index List: {}".format(self.turn_indexlist))
-        for t in self.turn_indexlist:
-            print(self.log_lines[t])
         print("Win Index: {}".format(self.win_index))
-        print(self.log_lines[(self.win_index)])
 
 
+    def get_initialteams(self):
+        """ 
+        Returns list of pokemon, Player1, Player2 
+        Requires splitting 
+        Logfile Line: 
+            |poke|p1|Excadrill, M|
+            |poke|p2|Ditto|
+        """
+
+        poke_lines = [self.log_lines[i] for i in self.teampreview_indexlist]
+        team_player1 = [p.split("|p1|")[-1].split("|")[0].split(",")[0] for p in poke_lines if "p1" in p]
+        team_player2 = [p.split("|p2|")[-1].split("|")[0].split(",")[0] for p in poke_lines if "p2" in p]
+
+        print("Player1: ", team_player1)
+        print("Player2: ", team_player2)
+
+        return team_player1, team_player2
